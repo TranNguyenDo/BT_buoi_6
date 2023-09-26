4bb4fa6c38dc03b78dda569cd7b45c86e915faa2
TranNguyenDo <145801570+TranNguyenDo@users.noreply.github.com>
2023-09-26 23:48:49 +0700

    Add files via upload

---
+++ Sun of numbers.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+    int n;
+    int sum = 0;
+
+    // Yêu cầu người dùng nhập giá trị nguyên dương
+    do {
+        std::cout << "Nhập một số nguyên dương: ";
+        std::cin >> n;
+
+        // Xác thực đầu vào: Kiểm tra nếu n là số nguyên dương
+        if (n <= 0) {
+            std::cout << "Số phải là một số nguyên dương. Vui lòng thử lại." << std::endl;
+        }
+    } while (n <= 0);
+
+    // Tính tổng từ 1 đến n
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    // In ra tổng
+    std::cout << "Tổng của các số từ 1 đến " << n << " là: " << sum << std::endl;
+
+    return 0;
+}
