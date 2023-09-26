844441c30a6e55b15028ad05e3dc8e497e7344b0
TranNguyenDo <145801570+TranNguyenDo@users.noreply.github.com>
2023-09-26 21:31:36 +0700

    Add files via upload

---
+++ program 5.22.cpp
@@ -0,0 +1,6 @@
+﻿Build started...
+1>------ Build started: Project: pr22, Configuration: Debug x64 ------
+1>pr22.cpp
+1>pr22.vcxproj -> C:\Users\Admin\source\repos\pr22\x64\Debug\pr22.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build started at 1:05 AM and took 01.883 seconds ==========
---
+++ program 5.5.cpp
@@ -0,0 +1,6 @@
+﻿Build started...
+1>------ Build started: Project: program 5.3, Configuration: Debug x64 ------
+1>program 5.3.cpp
+1>program 5.3.vcxproj -> C:\Users\Admin\source\repos\program 5.3\x64\Debug\program 5.3.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build started at 11:40 PM and took 01.735 seconds ==========
